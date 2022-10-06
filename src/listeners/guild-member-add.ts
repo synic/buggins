ca4569f39d2bafabbbad74df9804cb5f7cfb2f71
Adam Olsen <arolsen@gmail.com>
2022-10-06 13:46:03 -0600

    Automagically give role on join

--- src/listeners/guild-member-add.ts
+++ src/listeners/guild-member-add.ts
@@ -3,5 +3,19 @@ import { Client, GuildMember } from 'discord.js';
 export default (client: Client): void => {
   client.on('guildMemberAdd', async (member: GuildMember) => {
     console.log(`${member.nickname} has joined`);
+
+    const role = member.guild.roles.cache.find(
+      (r) => r.name.toLowerCase() === 'photographers',
+    );
+    if (!role) {
+      console.log('Could not find photographers role, not adding.');
+      return;
+    }
+
+    try {
+      await member.roles.add(role);
+    } catch (error) {
+      console.log(`error adding user to role: ${error}`);
+    }
   });
 };
