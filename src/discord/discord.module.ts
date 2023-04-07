1f40f71ff55ea09dee12021bf30972c14e0797d7
Adam Olsen <arolsen@gmail.com>
2023-04-07 09:36:50 -0600

    Remove unused intents

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,7 +1,7 @@
 import { ConfigModule, ConfigType } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import discordConfig from './discord.config';
-import { GatewayIntentBits, Partials } from 'discord.js';
+import { GatewayIntentBits } from 'discord.js';
 import { DiscordModule as BaseDiscordModule } from '@discord-nestjs/core';
 import { DiscordService } from './discord.service';
 
@@ -15,14 +15,10 @@ import { DiscordService } from './discord.service';
         token: config.token,
         discordClientOptions: {
           intents: [
-            GatewayIntentBits.GuildEmojisAndStickers,
-            GatewayIntentBits.GuildMembers,
             GatewayIntentBits.GuildMessages,
-            GatewayIntentBits.GuildMessageReactions,
             GatewayIntentBits.Guilds,
             GatewayIntentBits.MessageContent,
           ],
-          partials: [Partials.Channel, Partials.Message, Partials.Reaction],
         },
         registerCommandOptions: [
           {
